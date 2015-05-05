3348e6b6f21523d68208d8abedcdda0a0822fd60
Piet Geursen <pietgeursen@gmail.com>
2015-05-05 15:24:25 +1200

    http!

--- program.js
+++ program.js
@@ -1,9 +1,12 @@
-var mod = require('./module.js')
+'use strict';
 
-dir = process.argv[2];
-ext = process.argv[3];
-mod(dir,ext, function(err, data){
-	data.forEach(function(file){console.log(file)});
-})
+var http = require('http');
 
+var url = process.argv[2];
 
+http.get(url, function (response) {
+	response.setEncoding('utf8');
+	response.on('data', function (data) {
+		console.log(data);
+	});
+});
