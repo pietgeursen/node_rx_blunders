3ea2ecdb15df795ba4c7e9da020aa7c8ae8c1df1
Piet Geursen <pietgeursen@gmail.com>
2015-05-05 15:51:07 +1200

    HTTP collect

--- program.es6
+++ program.es6
@@ -1,10 +1,15 @@
 let http = require('http')
+let concat = require('concat-stream')
 
 let url = process.argv[2];
 
 http.get( url, response => {
 	response.setEncoding('utf8')
-	response.on('data', data =>{
+	response.pipe(concat((err,data) => {
+		if(err)
+			return console.error(err);
+		console.log(data.length);
 		console.log(data);
-	} )
+	}));
+
 })
--- program.js
+++ program.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var http = require('http');
+var concat = require('concat-stream');
 
 var url = process.argv[2];
 
 http.get(url, function (response) {
 	response.setEncoding('utf8');
-	response.on('data', function (data) {
+	response.pipe(concat(function (err, data) {
+		if (err) return console.error(err);
+		console.log(data.length);
 		console.log(data);
-	});
+	}));
 });
