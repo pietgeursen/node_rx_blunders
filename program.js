297a315dbc8ae3e5682f852a4b105cb50fcef058
Piet Geursen <pietgeursen@gmail.com>
2015-05-05 13:18:17 +1200

    Filtered LS

--- program.js
+++ program.js
@@ -5,7 +5,8 @@ var path = process.argv[2];
 var extension = process.argv[3];
 
 fs.readdir(path, function(err,data){
-	var files = data.filter(function(file){return p.extname(file) === extension})
-	console.log(files);	
+	var files = data.filter(function(file){return p.extname(file) === ('.' + extension)})
+	files.forEach(function(file){	console.log(file)	});
+
 })
 
