a54e4e673a31bf5185322a5da0295ce45df00ee4
Piet Geursen <pietgeursen@gmail.com>
2015-05-06 18:26:43 +1200

    RX FTW bitches

--- program.es6
+++ program.es6
@@ -1,18 +1,29 @@
 let http = require('http');
-let concat = require('concat-stream');
-let async = require('async');
-let fs = require('fs');
+// let concat = require('concat-stream');
+// let async = require('async');
+// let fs = require('fs');
+let Rx = require('rx');
 
 let port = process.argv[2];
-let file = process.argv[3]
-
-
+let file = process.argv[3];
 
 if(port){
 
 	let server = http.createServer((req, res) => {
-	let	src = fs.createReadStream(file);
-		src.pipe(res);
+
+		if(req.method === 'POST'){
+			req.on('end' , () => res.end())
+			let source = Rx.Observable.fromEvent(req, 'data');
+			let upCased = source.map(s => {
+				return s.toString().toUpperCase();
+			});
+			let subscription = upCased.subscribe(
+				e => {
+					res.write(e);
+				}
+			);
+			
+		}		
 	}).listen(port)
 	
 }
--- program.js
+++ program.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var http = require('http');
-var concat = require('concat-stream');
-var async = require('async');
-var fs = require('fs');
+// let concat = require('concat-stream');
+// let async = require('async');
+// let fs = require('fs');
+var Rx = require('rx');
 
 var port = process.argv[2];
 var file = process.argv[3];
@@ -11,7 +12,18 @@ var file = process.argv[3];
 if (port) {
 
 	var server = http.createServer(function (req, res) {
-		var src = fs.createReadStream(file);
-		src.pipe(res);
+
+		if (req.method === 'POST') {
+			req.on('end', function () {
+				return res.end();
+			});
+			var source = Rx.Observable.fromEvent(req, 'data');
+			var upCased = source.map(function (s) {
+				return s.toString().toUpperCase();
+			});
+			var subscription = upCased.subscribe(function (e) {
+				res.write(e);
+			});
+		}
 	}).listen(port);
 }
